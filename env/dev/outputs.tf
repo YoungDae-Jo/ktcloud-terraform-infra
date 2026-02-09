906902c242f4305e6816bac9c930bb1c96d63481
YoungDaeJo <qscwdv960@naver.com>
2026-02-09 17:14:50 +0900

    week1: finalize infra, backend, outputs, docs

--- env/dev/outputs.tf
+++ env/dev/outputs.tf
@@ -1,40 +1,55 @@
-output "project_name" {
-  value = var.project_name
-}
+############################################
+# VPC
+############################################
 
 output "vpc_id" {
-  value = module.network.vpc_id
+  description = "VPC ID"
+  value       = module.network.vpc_id
 }
 
+############################################
+# Subnets
+############################################
+
 output "public_subnet_ids" {
-  value = module.network.public_subnet_ids
+  description = "Public Subnet IDs"
+  value       = module.network.public_subnet_ids
 }
 
 output "private_subnet_ids" {
-  value = module.network.private_subnet_ids
+  description = "Private Subnet IDs"
+  value       = module.network.private_subnet_ids
 }
 
-output "internet_gateway_id" {
-  value = module.network.internet_gateway_id
-}
-
-output "public_route_table_id" {
-  value = module.network.public_route_table_id
-}
+############################################
+# ALB
+############################################
 
 output "alb_dns_name" {
-  value = module.alb.alb_dns_name
+  description = "ALB DNS Name"
+  value       = module.alb.alb_dns_name
 }
 
 output "target_group_arn" {
-  value = module.alb.target_group_arn
+  description = "Target Group ARN"
+  value       = module.alb.target_group_arn
 }
+
+############################################
+# ASG
+############################################
 
 output "asg_name" {
-  value = module.asg.asg_name
+  description = "Auto Scaling Group Name"
+  value       = module.asg.asg_name
 }
 
-output "launch_template_id" {
-  value = module.asg.launch_template_id
+############################################
+# Monitoring
+############################################
+
+output "monitoring_public_ip" {
+  description = "Monitoring EC2 Public IP"
+  value       = module.monitoring.public_ip
 }
 
